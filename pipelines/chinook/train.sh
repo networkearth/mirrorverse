3500cc24a963b1cf07c819690c8b6324198d9eec
erebuseternal <marcelsanders96@gmail.com>
2024-03-15 17:12:48 +0000

    trained chinook

---
+++ pipelines/chinook/train.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+train_chinook -d data/tag_tracks.csv -t data/surface_temps.csv -e data/bathymetry.csv -m models.pkl
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mirrorverse",
+    version="0.0.1",
+    author="Marcel Gietzmann-Sanders",
+    author_email="marcelsanders96@gmail.com",
+    packages=find_packages(include=["mirrorverse", "mirrorverse*"]),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "train_chinook=mirrorverse.chinook.train:main",
+        ]
+    },
+)
