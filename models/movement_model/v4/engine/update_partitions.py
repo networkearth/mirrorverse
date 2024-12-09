e94d5a739bb6f038eba3290681f2c7f2d4d0ccf1
erebuseternal <marcelsanders96@gmail.com>
2024-12-09 21:46:38 +0000

    wip

--- models/movement_model/v4/engine/update_partitions.py
+++ models/movement_model/v4/engine/update_partitions.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     os.environ['AWS_PROFILE'] = 'admin'
     os.environ['HAVEN_DATABASE'] = 'haven'
 
-    db.register_partitions('spark_test_9', public_internet_access=True)
+    db.register_partitions('movement_model_simulation_v3_s2', public_internet_access=True)
     db.register_partitions('spark_test_10', public_internet_access=True)
