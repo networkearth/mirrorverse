10c7e92f6d45f8a35cda5f0cae10d41a87620528
erebuseternal <marcelsanders96@gmail.com>
2025-02-03 13:57:26 +0000

    cleanup

--- models/movement_model/epicurean/engine/update_partitions.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import haven.spark as db
-
-if __name__ == '__main__':
-    os.environ['AWS_PROFILE'] = 'admin'
-    os.environ['HAVEN_DATABASE'] = 'haven'
-
-    db.register_partitions('movement_model_raw_features_m2_a2', public_internet_access=True)
